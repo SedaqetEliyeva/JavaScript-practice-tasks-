3a48e1d40d329dcfc0483a1f99feb7a4d08dcaa0
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2024-02-04 23:12:33 +0300

    master

---
+++ Tasks.js
@@ -0,0 +1,17 @@
+let num=19;//number
+const str="Sadaqat";//string
+const isStudent=true;//boolean
+const isEmpty=null;//nul
+const isDefined=undefined;//undefined
+console.log(isStudent)
+
+//!!--as it is
+//!--the opposite
+const a=!!true;
+const b=!true;
+
+//comparison of null and undefined:
+//null--exists but is empty
+//undefined--does not exist
+//example:
+let result;//undefined
