973f9ae3db36467de5f62b7d21fc2a52481c964e
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2024-02-05 22:01:20 +0300

    numbers

--- Tasks.js
+++ Tasks.js
@@ -1,22 +1,8 @@
-const number1=5;
-const number2=8;
-const number3=3;
+const number=5;
 
-if(number1>number2 & number1>number3){
-    console.log(number1);
+if(number%2){
+    console.log("tek");
 }
-else if(number2>number1 & number2>number3){
-    console.log(number2);
-}
-else if(number3>number1 & number3>number2){
-    console.log(number3);
-}
-else if(number1==number2 & number3>number1){
-    console.log(number3);
-}
-else if(number1==number3 & number2>number1){
-    console.log(number2);
-}
-else if(number2==number3 & number1>number2){
-    console.log(number1);
+else{
+    console.log("cut")
 }
