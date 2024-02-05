62d2f0aaff87b66c50b01dc28ea510f1590f3dab
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2024-02-05 14:40:27 +0300

    numbers

--- Tasks.js
+++ Tasks.js
@@ -1,4 +1,22 @@
-const v=60/3.6;//--In the international system
-const S=180*1000;//--In the international system
-const t=(S/v)/60;//--minute
-console.log(t, "minute")
+const number1=5;
+const number2=8;
+const number3=3;
+
+if(number1>number2 & number1>number3){
+    console.log(number1);
+}
+else if(number2>number1 & number2>number3){
+    console.log(number2);
+}
+else if(number3>number1 & number3>number2){
+    console.log(number3);
+}
+else if(number1==number2 & number3>number1){
+    console.log(number3);
+}
+else if(number1==number3 & number2>number1){
+    console.log(number2);
+}
+else if(number2==number3 & number1>number2){
+    console.log(number1);
+}
