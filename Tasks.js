8c7b0d5c1eff281a9a065572ac141af58aa41545
Sadaqat Aliyeva <148767334+SedaqetEliyeva@users.noreply.github.com>
2024-02-08 11:29:51 +0300

    Tasks.js

--- Tasks.js
+++ Tasks.js
@@ -2,21 +2,21 @@ const number1=5;
 const number2=8;
 const number3=3;
 
-if(number1>number2 & number1>number3){
+if(number1>number2 && number1>number3){
     console.log(number1);
 }
-else if(number2>number1 & number2>number3){
+else if(number2>number1 && number2>number3){
     console.log(number2);
 }
-else if(number3>number1 & number3>number2){
+else if(number3>number1 && number3>number2){
     console.log(number3);
 }
-else if(number1==number2 & number3>number1){
+else if(number1==number2 && number3>number1){
     console.log(number3);
 }
-else if(number1==number3 & number2>number1){
+else if(number1==number3 && number2>number1){
     console.log(number2);
 }
-else if(number2==number3 & number1>number2){
+else if(number2==number3 && number1>number2){
     console.log(number1);
 }
