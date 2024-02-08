d2e01c0580cf0da074ed615bf74ae43a32d4c809
Sadaqat Aliyeva <148767334+SedaqetEliyeva@users.noreply.github.com>
2024-02-08 11:35:57 +0300

    Task3.js

--- Task3.js
+++ Task3.js
@@ -1,6 +1,8 @@
-const name="Sadaqat"
-const sourname="Aliyeva"
-const fullname=name + sourname
-const fullname2=name + " " + sourname
-console.log(fullname)
-console.log(fullname2)
+const name="Sadaqat";
+const sourname="Aliyeva";
+
+const fullname=name + sourname;
+const fullname2=name + " " + sourname;
+
+console.log(fullname);
+console.log(fullname2);
