c4ccbf5466b6d12a8df20298a1f7b15ab8c7c99d
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2024-02-08 21:25:56 +0300

    master

--- Tasks.js
+++ Tasks.js
@@ -1,5 +1,364 @@
-const number=25;
+const number=20;
 
-if(number){
-    console.log(true)
+for(let i=0; number-(number%10); i++){
+    if(number-(number%10)==10 && number%10==0){
+        console.log("ten");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==1){
+        console.log("eleven");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==2){
+        console.log("twelve");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==3){
+        console.log("thirteen");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==4){
+        console.log("fourteen");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==5){
+        console.log("fifteen");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==6){
+        console.log("sixteen");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==7){
+        console.log("seventeen");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==8){
+        console.log("eighteen");
+        break;
+    }
+    else if(number-(number%10)==10 && number%10==9){
+        console.log("nineteen");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==0){
+        console.log("twenty");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==1){
+        console.log("twenty one");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==2){
+        console.log("twenty two");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==3){
+        console.log("twenty three");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==4){
+        console.log("twenty four");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==5){
+        console.log("twenty five");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==6){
+        console.log("twenty six");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==7){
+        console.log("twenty seven");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==8){
+        console.log("twenty eight");
+        break;
+    }
+    else if(number-(number%10)==20 && number%10==9){
+        console.log("twenty nine");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==0){
+        console.log("thirty");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==1){
+        console.log("thirty one");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==2){
+        console.log("thirty two");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==3){
+        console.log("thirty three");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==4){
+        console.log("thirty four");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==5){
+        console.log("thirty five");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==6){
+        console.log("twenty six");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==7){
+        console.log("thirty seven");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==8){
+        console.log("twenty eight");
+        break;
+    }
+    else if(number-(number%10)==30 && number%10==9){
+        console.log("thirty nine");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==0){
+        console.log("forty");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==1){
+        console.log("forty one");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==2){
+        console.log("forty two");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==3){
+        console.log("forty three");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==4){
+        console.log("forty four");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==5){
+        console.log("forty five");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==6){
+        console.log("forty six");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==7){
+        console.log("forty seven");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==8){
+        console.log("forty eight");
+        break;
+    }
+    else if(number-(number%10)==40 && number%10==9){
+        console.log("forty nine");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==0){
+        console.log("fifty");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==1){
+        console.log("fifty one");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==2){
+        console.log("fifty two");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==3){
+        console.log("fifty three");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==4){
+        console.log("fifty four");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==5){
+        console.log("fifty five");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==6){
+        console.log("fifty six");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==7){
+        console.log("fifty seven");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==8){
+        console.log("fifty eight");
+        break;
+    }
+    else if(number-(number%10)==50 && number%10==9){
+        console.log("fifty nine");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==0){
+        console.log("sixty");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==1){
+        console.log("sixty one");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==2){
+        console.log("sixty two");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==3){
+        console.log("sixty three");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==4){
+        console.log("sixty four");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==5){
+        console.log("sixty five");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==6){
+        console.log("sixty six");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==7){
+        console.log("sixty seven");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==8){
+        console.log("sixty eight");
+        break;
+    }
+    else if(number-(number%10)==60 && number%10==9){
+        console.log("sixty nine");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==0){
+        console.log("seventy");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==1){
+        console.log("seventy one");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==2){
+        console.log("seventy two");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==3){
+        console.log("seventy three");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==4){
+        console.log("seventy four");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==5){
+        console.log("seventy five");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==6){
+        console.log("seventy six");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==7){
+        console.log("seventy seven");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==8){
+        console.log("seventy eight");
+        break;
+    }
+    else if(number-(number%10)==70 && number%10==9){
+        console.log("seventy nine");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==0){
+        console.log("eighty");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==1){
+        console.log("eighty one");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==2){
+        console.log("eighty two");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==3){
+        console.log("eighty three");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==4){
+        console.log("eighty four");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==5){
+        console.log("eighty five");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==6){
+        console.log("eighty six");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==7){
+        console.log("eighty seven");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==8){
+        console.log("eighty eight");
+        break;
+    }
+    else if(number-(number%10)==80 && number%10==9){
+        console.log("eighty nine");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==0){
+        console.log("ninety");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==1){
+        console.log("ninety one");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==2){
+        console.log("ninety two");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==3){
+        console.log("ninety three");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==4){
+        console.log("ninety four");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==5){
+        console.log("ninety five");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==6){
+        console.log("ninety six");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==7){
+        console.log("ninety seven");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==8){
+        console.log("ninety eight");
+        break;
+    }
+    else if(number-(number%10)==90 && number%10==9){
+        console.log("ninety nine");
+        break;
+    }
 }
