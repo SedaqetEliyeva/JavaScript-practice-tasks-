11c0d66bb0351f1586a9d594dc1dd82576a1b58e
Sadaqat Aliyeva <148767334+SedaqetEliyeva@users.noreply.github.com>
2024-02-08 11:34:59 +0300

    Task2.js

--- Task2.js
+++ Task2.js
@@ -1,7 +1,9 @@
-let name="Sadaqat"
-let sourname="Aliyeva"
-let old="19"
-let fullname=name + sourname
-console.log(fullname, old)
-console.log(name, sourname, old)
-console.log(name + " " + sourname)
+let name="Sadaqat";
+let sourname="Aliyeva";
+let old="19";
+
+let fullname=name + sourname;
+
+console.log(fullname, old);
+console.log(name, sourname, old);
+console.log(name + " " + sourname);
