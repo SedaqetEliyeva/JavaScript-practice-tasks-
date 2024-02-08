481e6fcaf08bc91153810e1467bae54b56a5b5ef
Sadaqat Aliyeva <148767334+SedaqetEliyeva@users.noreply.github.com>
2024-02-08 11:24:48 +0300

    Tasks.js

--- Tasks.js
+++ Tasks.js
@@ -1,5 +1,7 @@
 const number=5;
+
 let number2=number**2;
 let number3=number**3;
-console.log("the square of the number:", number2) 
-console.log("the cube of a number:", number3)
+
+console.log("the square of the number:", number2);
+console.log("the cube of a number:", number3);
