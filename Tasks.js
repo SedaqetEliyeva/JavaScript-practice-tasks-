8a2c3032c4dc9b5cc032e437fc7996244cc433b1
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2024-02-09 08:47:50 +0300

    numbers

--- Tasks.js
+++ Tasks.js
@@ -1,21 +1,3 @@
-const number=1000;
-
-for(let i=0; i<=1000; i++){
-    let s= number-(number%10)
-    if(s>=10 && s<=90){
-        console.log("is a two digit number");
-        break;
-    }
-    else if(s>=100 && s<=990){
-        console.log("is a three digit number")
-        break;
-    }
-    else if(s==1000){
-        console.log("is a four digit number");
-        break;
-    }
-    else{
-        console.log("is a single digit number");
-        break;
-    }
+for(let i=0; i<10; i++){
+    console.log("Hello Wold");
 }
