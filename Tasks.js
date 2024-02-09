64d1257a52cfdf4c4ebd47c2803fd92932d092f1
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2024-02-09 13:11:38 +0300

    bonus

--- Tasks.js
+++ Tasks.js
@@ -4,3 +4,12 @@ for(let i=0; i<5; i++){
     result=result+"x";
     console.log(result);
 }
+
+
+for(let i=5; i>0; i--){
+    result=" "
+    for(let j=0; j<i; j++){
+        result=result+"x";
+    }
+    console.log(result);
+}
