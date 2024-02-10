cf69b2ae03ff2a35c0cafcb79c43a860d00c4113
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2024-02-10 11:43:13 +0300

    bonus

---
+++ Task2.js
@@ -0,0 +1,12 @@
+let bool=false;
+let value=0;
+
+while(value<5){
+    value++;
+    console.log(bool);
+}
+
+console.log(value);
+
+//let bool=false -- stops
+//let bool=true -- executes an infinite number of times
